a276bd5c4df27b844ccd62b19ab2bcb42c5994b2
iuncuim <iuncuim@gmail.com>
2016-12-05 17:06:03 +0300

    Удален U_BOOT_VERSION из-за *-dirty

--- include/version.h
+++ include/version.h
@@ -18,7 +18,7 @@
 #define CONFIG_IDENT_STRING ""
 #endif
 
-#define U_BOOT_VERSION_STRING "2014.10 Build TELEOFIS 1.2.0" CONFIG_IDENT_STRING /* fix it */
+#define U_BOOT_VERSION_STRING "U-Boot 2014.10 Build TELEOFIS 1.2.0" CONFIG_IDENT_STRING /* fix it */
 
 #ifndef __ASSEMBLY__
 extern const char version_string[];
